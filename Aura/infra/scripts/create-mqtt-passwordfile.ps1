424a2fab78abbacd1baacdd1ca73cc2adb478304
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-08-25 16:35:27 +0530

    Delete Aura directory

--- Aura/infra/scripts/create-mqtt-passwordfile.ps1
+++
@@ -1,9 +0,0 @@
-param(
-  [Parameter(Mandatory = $true)][string]$DevicePassword,
-  [Parameter(Mandatory = $true)][string]$BackendPassword
-)
-
-$target = Join-Path $PSScriptRoot "..\mosquitto\passwordfile"
-docker run --rm -v "$(Resolve-Path (Join-Path $PSScriptRoot '..\mosquitto')):/mosquitto/config" eclipse-mosquitto:2 `
-  sh -c "mosquitto_passwd -b -c /mosquitto/config/passwordfile esp32_01 '$DevicePassword' && mosquitto_passwd -b /mosquitto/config/passwordfile somnus_backend '$BackendPassword'"
-Write-Host "Created $target. Keep it private."
